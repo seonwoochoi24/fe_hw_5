3aeb85484df03e4d1f7908fecfaa8a841acf01aa
seonwoochoi24 <seonwoo31@dgu.ac.kr>
2025-05-13 00:31:12 +0900

    js과제

---
+++ myTag/index.js
@@ -0,0 +1,14 @@
+//index.js
+// index.js 맨 위에 추가
+const customTag = require('./main');
+
+customTag.changeTagName('P');
+customTag.changeTextContent('아기사자');
+customTag.changeStyles('color', 'red');
+customTag.changeStyles('fontSize', '20px');
+customTag.setId('myId');
+customTag.addClassName('myClass1');
+customTag.addClassName('myClass2');
+customTag.removeClassName('myClass2');
+
+console.log(customTag.toHTML());
